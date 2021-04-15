c0fc76085ddf980d9fb309f8be6f2e0170ab84f4
VeroMunoz <veronica.munoz@wizeline.com>
2021-04-15 16:53:40 -0500

    Example with python

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import Add 
+def TestAdd():
+    # Function assert: ensure that it always be true
+    assert Add(2,3) == 5
+    print ("Add function works correctly")
+
+if __name__ == '__main__':
+    TestAdd()
