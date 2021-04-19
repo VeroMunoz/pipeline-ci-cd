873f87687faed8f34741623232b96a4a3f44a1a5
VeroMunoz <veronica.munoz@wizeline.com>
2021-04-19 17:43:44 -0500

    cancel workflows

--- circle.sh
+++ circle.sh
@@ -3,9 +3,8 @@ echo $other_id
 
 for other_id in $other_id
 do
+    workflow_id=$(curl -s --header "Circle-Token: $MyToken" --request GET "https://circleci.com/api/v2/pipeline/${other_id}/workflow" | jq -r '.items[]|select(.status == "on_hold").id')
+    echo $workflow_id 
 
-    #workflow_id=$(curl -s --header "Circle-Token: $MyToken" --request GET "https://circleci.com/api/v2/pipeline/${other_id}/workflow" | jq -r '.items[]|select(.status == "on_hold").id')
-    #echo $workflow_id
-    i=0
     #curl --header "Circle-Token: $TOKEN"  --request POST 'https://circleci.com/api/v2/workflow/d287ee15-0ecd-4378-a58c-bbb3b857a60e/cancel'
 done
