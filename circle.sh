fa187e9b60c9f17d29d421a0898f6b6dd7a34686
VeroMunoz <veronica.munoz@wizeline.com>
2021-04-20 11:50:23 -0500

    Fix for

--- circle.sh
+++ circle.sh
@@ -1,8 +1,10 @@
- pipeline_id=$(curl -s -X GET 'https://circleci.com/api/v2/project/gh/VeroMunoz/pipeline-ci-cd/pipeline?circle-token=${TOKEN}' | jq -r --arg CIRCLE_USERNAME "$CIRCLE_USERNAME" '.items[]|select(.state == "created").id')
+pipeline_id=$(curl -s -X GET 'https://circleci.com/api/v2/project/gh/VeroMunoz/pipeline-ci-cd/pipeline?circle-token=${TOKEN}' | jq -r --arg CIRCLE_USERNAME "$CIRCLE_USERNAME" '.items[]|select(.state == "created").id')
 echo $pipeline_id  
 for pipeline_id in $pipeline_id
     do
     workflow_id=$(curl -s -X GET "https://circleci.com/api/v2/pipeline/${pipeline_id}/workflow" | jq -r '.items[]|select(.status == "on_hold").id')
     echo $workflow_id
-    curl -s --header "Circle-Token: $PERSONAL_TOKEN" -X POST https://circleci.com/api/v2/workflow/${workflow_id}/cancel
+    if [ ! -z "$workflow_id" ]; then
+        curl -s --header "Circle-Token: $PERSONAL_TOKEN" -X POST https://circleci.com/api/v2/workflow/${workflow_id}/cancel
+    fi
     done 
